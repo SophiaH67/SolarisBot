1d7b0446b1f25a8859fb39fd3230461406803adb
Paci <89483951+PaciStardust@users.noreply.github.com>
2024-03-28 09:37:17 +0100

    Soft delete for regex channels

--- Database/Models/DbRegexChannel.cs
+++ Database/Models/DbRegexChannel.cs
@@ -18,9 +18,15 @@ namespace SolarisBot.Database
         public string PunishmentMessage { get; set; } = string.Empty;
         public bool PunishmentDelete { get; set; } = false;
         public ulong PunishmentTimeout { get; set; } = ulong.MinValue;
-        public bool IsDeleted { get; set; } = false; //todo: impl
+        public bool IsDeleted { get; set; } = false; //todo: [TEST] Do new DB Constraints and soft delete work?
 
         public override string ToString() //todo: [Logging] Add better ToString?
             => Regex;
+    }
+
+    internal static class DbRegexChannelExtensions
+    {
+        internal static IQueryable<DbRegexChannel> IsDeleted(this IQueryable<DbRegexChannel> query, bool isDeleted)
+            => query.Where(x => x.IsDeleted == isDeleted);
     }
 }
