5c8a34c4ecbb548576dfdd519f14742e21609867
Paci <89483951+PaciStardust@users.noreply.github.com>
2024-06-02 12:41:03 +0200

    Implemented RegexChannel Service

--- Discord/Modules/Fun/RegexChannelService.cs
+++ Discord/Modules/Fun/RegexChannelService.cs
@@ -1,12 +1,13 @@
-﻿using Discord.WebSocket;
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SolarisBot.Database;
+using SolarisBot.Discord.Common;
 using SolarisBot.Discord.Common.Attributes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace SolarisBot.Discord.Modules.Fun
 {
@@ -36,9 +37,100 @@ namespace SolarisBot.Discord.Modules.Fun
             return Task.CompletedTask;
         }
 
-        private Task CheckForRegexAsync(SocketMessage arg)
+        private async Task CheckForRegexAsync(SocketMessage message)
         {
-            return Task.CompletedTask;
+            if (message is not IUserMessage userMessage || message.Author.IsWebhook || message.Author.IsBot || message.Author is not IGuildUser gUser)
+                return;
+
+            var dbCtx = _provider.GetRequiredService<DatabaseContext>();
+            var regexChannel = await dbCtx.RegexChannels.ForChannel(message.Channel.Id).FirstOrDefaultAsync();
+
+            if (regexChannel is null)
+                return;
+
+            try
+            {
+                if (Regex.IsMatch(message.CleanContent, regexChannel.Regex))
+                    return;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to compile regex {regex}", regexChannel);
+                return;
+            }
+
+            if (!string.IsNullOrWhiteSpace(regexChannel.PunishmentMessage))
+            {
+                try
+                {
+                    if (regexChannel.PunishmentDelete)
+                    {
+                        _logger.LogDebug("Sending message to regex {regex} violation by user {user} in channel {channel} of guild {guild} with message {message}", regexChannel, message.Author.Log(), message.Channel.Log(), gUser.Guild.Log(), message.CleanContent);
+                        await message.Channel.SendMessageAsync($"{message.Author.Mention} {regexChannel.PunishmentMessage}");
+                        _logger.LogInformation("Sent message to regex {regex} violation by user {user} in channel {channel} of guild {guild} with message {message}", regexChannel, message.Author.Log(), message.Channel.Log(), gUser.Guild.Log(), message.CleanContent);
+                    }
+                    else
+                    {
+                        _logger.LogDebug("Responding to regex {regex} violation by user {user} in channel {channel} of guild {guild} with message {message}", regexChannel, message.Author.Log(), message.Channel.Log(), gUser.Guild.Log(), message.CleanContent);
+                        await userMessage.ReplyAsync(regexChannel.PunishmentMessage);
+                        _logger.LogInformation("Responded to regex {regex} violation by user {user} in channel {channel} of guild {guild} with message {message}", regexChannel, message.Author.Log(), message.Channel.Log(), gUser.Guild.Log(), message.CleanContent);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Failed to reply to regex {regex} violation by user {user} in channel {channel} of guild {guild} with message {message}", regexChannel, message.Author.Log(), message.Channel.Log(), gUser.Guild.Log(), message.CleanContent);
+                }
+            }
+
+            if (regexChannel.PunishmentDelete)
+            {
+                try
+                {
+                    _logger.LogDebug("Deleting message of regex {regex} violation by user {user} in channel {channel} of guild {guild} with message {message}", regexChannel, message.Author.Log(), message.Channel.Log(), gUser.Guild.Log(), message.CleanContent);
+                    await message.DeleteAsync();
+                    _logger.LogInformation("Deleted message of regex {regex} violation by user {user} in channel {channel} of guild {guild} with message {message}", regexChannel, message.Author.Log(), message.Channel.Log(), gUser.Guild.Log(), message.CleanContent);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Failed to delete message of regex {regex} violation by user {user} in channel {channel} of guild {guild} with message {message}", regexChannel, message.Author.Log(), message.Channel.Log(), gUser.Guild.Log(), message.CleanContent);
+                }
+            }
+
+            if (regexChannel.PunishmentTimeout > 0)
+            {
+                try
+                {
+                    _logger.LogDebug("Timing out for regex {regex} violation by user {user} in channel {channel} of guild {guild} with message {message}", regexChannel, message.Author.Log(), message.Channel.Log(), gUser.Guild.Log(), message.CleanContent);
+                    await gUser.SetTimeOutAsync(TimeSpan.FromSeconds(regexChannel.PunishmentTimeout));
+                    _logger.LogInformation("Timed out for regex {regex} violation by user {user} in channel {channel} of guild {guild} with message {message}", regexChannel, message.Author.Log(), message.Channel.Log(), gUser.Guild.Log(), message.CleanContent);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Failed timing out for regex {regex} violation by user {user} in channel {channel} of guild {guild} with message {message}", regexChannel, message.Author.Log(), message.Channel.Log(), gUser.Guild.Log(), message.CleanContent);
+                }
+            }
+
+            if (regexChannel.AppliedRoleId > 0)
+            {
+                var role = gUser.Guild.FindRole(regexChannel.AppliedRoleId);
+                if (role is null) //todo: [FEATURE] Notify for this?
+                {
+                    return;
+                }
+                else
+                {
+                    try
+                    {
+                        _logger.LogDebug("Applying role {role} for regex {regex} violation by user {user} in channel {channel} of guild {guild} with message {message}", role.Log(), regexChannel, message.Author.Log(), message.Channel.Log(), gUser.Guild.Log(), message.CleanContent);
+                        await gUser.AddRoleAsync(role);
+                        _logger.LogInformation("Applied role {role} for regex {regex} violation by user {user} in channel {channel} of guild {guild} with message {message}", role.Log(), regexChannel, message.Author.Log(), message.Channel.Log(), gUser.Guild.Log(), message.CleanContent);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, "Failed applying role {role} for regex {regex} violation by user {user} in channel {channel} of guild {guild} with message {message}", role.Log(), regexChannel, message.Author.Log(), message.Channel.Log(), gUser.Guild.Log(), message.CleanContent);
+                    }
+                }
+            }
         }
     }
 }
